e900cf9f2011f07ebab4bf39387b07e7fe8e0251
Halit Guven Serin <halitguvens@gmail.com>
2021-10-18 17:32:04 +0300

    Fix hide header component in login,signin bug

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,80 +4,84 @@ import { Wrapper } from "./ScHeader";
 import { AiOutlinePlus, AiOutlineUser, AiOutlineForm } from "react-icons/ai";
 import { BiLogIn } from "react-icons/bi";
 import { Link } from "react-router-dom";
-import { useLocation } from "react-router";
 
 import { ACCESS_TOKEN_NAME } from "../../api";
-
+import { history } from "../../_helpers/history";
+import { useLocation } from "react-router";
 const Header = () => {
   const token = localStorage.getItem(ACCESS_TOKEN_NAME);
-  const location = useLocation();
 
   // eslint-disable-next-line no-unused-vars
   const [isAuthenticated, setIsAuthenticated] = useState(true);
 
+  const location = useLocation();
+
   const handleLogout = () => {
     localStorage.removeItem(ACCESS_TOKEN_NAME);
+    localStorage.removeItem("email");
     setIsAuthenticated(false);
+    history.push("/");
   };
 
   if (location.pathname === "/sign-up" || location.pathname === "/sign-in")
     return null;
-  return (
-    <Wrapper>
-      <div>
-        <Link to="/">
-          <img src={logo} alt="İkinci El" />
-        </Link>
-      </div>
-      <div className="buttons">
-        {token ? (
-          <>
-            <button>
-              <div>
-                <AiOutlinePlus />
-                <span>Ürün Ekle</span>
-              </div>
-            </button>
-            <Link to="/profile">
+  else
+    return (
+      <Wrapper>
+        <div>
+          <Link to="/">
+            <img src={logo} alt="İkinci El" />
+          </Link>
+        </div>
+        <div className="buttons">
+          {token ? (
+            <>
               <button>
                 <div>
-                  <AiOutlineUser />
-                  <span>Hesabım</span>
+                  <AiOutlinePlus />
+                  <span>Ürün Ekle</span>
                 </div>
               </button>
-            </Link>
-            <button onClick={handleLogout}>
+              <Link to="/profile">
+                <button>
+                  <div>
+                    <AiOutlineUser />
+                    <span>Hesabım</span>
+                  </div>
+                </button>
+              </Link>
+              <button onClick={handleLogout}>
+                <div>
+                  <AiOutlineUser />
+                  <span>Çıkış Yap</span>
+                </div>
+              </button>
+            </>
+          ) : (
+            <>
               <div>
-                <AiOutlineUser />
-                <span>Çıkış Yap</span>
+                <Link to="/sign-in">
+                  <button>
+                    <div>
+                      <BiLogIn />
+                      <span>Giriş Yap</span>
+                    </div>
+                  </button>
+                </Link>
+                <Link to="/sign-up">
+                  <button>
+                    <div>
+                      <AiOutlineForm />
+                      <span>Üye Ol</span>
+                    </div>
+                  </button>
+                </Link>
               </div>
-            </button>
-          </>
-        ) : (
-          <>
-            <div>
-              <Link to="/sign-in">
-                <button>
-                  <div>
-                    <BiLogIn />
-                    <span>Giriş Yap</span>
-                  </div>
-                </button>
-              </Link>
-              <Link to="/sign-up">
-                <button>
-                  <div>
-                    <AiOutlineForm />
-                    <span>Üye Ol</span>
-                  </div>
-                </button>
-              </Link>
-            </div>
-          </>
-        )}
-      </div>
-    </Wrapper>
-  );
+            </>
+          )}
+        </div>
+      </Wrapper>
+    );
 };
 
 export default Header;
