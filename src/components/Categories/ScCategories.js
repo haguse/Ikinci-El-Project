cd81c0c1172bc500dadde52b67911dc84255719e
Halit Guven Serin <halitguvens@gmail.com>
2021-10-18 19:18:24 +0300

    Update categories css

--- src/components/Categories/ScCategories.js
+++ src/components/Categories/ScCategories.js
@@ -5,5 +5,15 @@ export const Wrapper = styled.div`
   ul {
     display: flex;
     justify-content: space-around;
+
+    @media only screen and (max-width: 1260px) {
+      justify-content: flex-start;
+      overflow-x: scroll;
+      overflow-y: hidden;
+      li  {
+        margin-right: 20px;
+        box-sizing: border-box;
+      }
+    }
   }
 `;
